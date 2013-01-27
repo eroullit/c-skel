02218e7496df9471f3a8b24e4b3615cbbaea6023
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-01-27 18:23:37 +0100

    replace O(2n) algo for O(1)
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- test/fibo.py
+++ test/fibo.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import sys
+import math
 import yaml
 
 def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+	return int(((1+math.sqrt(5))**n-(1-math.sqrt(5))**n)/(2**n*math.sqrt(5)))
 
 print yaml.dump(map(fib, range(int(sys.argv[1])+1)), default_flow_style=False, explicit_start=True)
